0c2454b5c715b415d4075bd939021b3728e522c9
mubeenimran <82304330+mubeenimran@users.noreply.github.com>
2021-05-21 23:22:06 +0500

    chkdskRestartScriptv1.0.1

--- Restartscript.bat
+++ Restartscript.bat
@@ -1,5 +1,9 @@
+set "params=%*"
+cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
+
+
 set /p input= "Do you want to run CHKDSK and Restart?"
 
 cls
 
-if %input%==y (fsutil dirty set c: && timeout /t 5 /nobreak && shutdown /r /t 000) else (exit /b)
+if %input%==y ( fsutil dirty set c: && timeout /t 5 /nobreak && shutdown /r /t 000) else (exit /b)
